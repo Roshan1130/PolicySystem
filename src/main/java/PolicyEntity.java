80c6e5ed764bdece1f7666d6dfc8bda2a3e6c1e5
roshan1130 <ramtel.1130@gmail.com>
2019-12-18 13:20:27 -0500

    1st commit: policy entity

---
+++ src/main/java/PolicyEntity.java
@@ -0,0 +1,83 @@
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="policies")
+public class PolicyEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name="type")
+	private String type;
+	
+	@Column(name="policyNo")
+	private Long policyNo;
+	
+	@Column(name="start")
+	private Date startDate;
+	
+	@Column(name="expire")
+	private Date expireDate;
+	
+	@Column(name="price")
+	private Integer price;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Long getPolicyNo() {
+		return policyNo;
+	}
+
+	public void setPolicyNo(Long policyNo) {
+		this.policyNo = policyNo;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getExpireDate() {
+		return expireDate;
+	}
+
+	public void setExpireDate(Date expireDate) {
+		this.expireDate = expireDate;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+	
+	
+}
