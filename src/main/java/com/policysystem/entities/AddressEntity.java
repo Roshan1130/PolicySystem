3258e755e23136b48366de89bcfbe289c4a91b73
roshan1130 <ramtel.1130@gmail.com>
2019-12-19 11:36:48 -0500

    updated entities

---
+++ src/main/java/com/policysystem/entities/AddressEntity.java
@@ -0,0 +1,11 @@
+package com.policysystem.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class AddressEntity {
+
+	
+}
---
+++ src/main/java/com/policysystem/entities/CustomerEntity.java
@@ -0,0 +1,96 @@
+package com.policysystem.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class CustomerEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="phone")
+	private String phone;
+	
+	@Column(name="gender")
+	private String gender;
+	
+	@Column(name="last_four_ssn")
+	private String lastFourSsn;
+	
+	@Column(name="dateOfBirth")
+	private Date date;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getLastFourSsn() {
+		return lastFourSsn;
+	}
+
+	public void setLastFourSsn(String lastFourSsn) {
+		this.lastFourSsn = lastFourSsn;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	
+}
